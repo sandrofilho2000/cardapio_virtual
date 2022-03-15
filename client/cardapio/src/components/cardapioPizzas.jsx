5a1d59389616b8a1e5aa5208184af77c00f24a23
Sandro Filho <sandrofilho98@yahoo.com.br>
2022-03-15 08:38:58 -0300

    new commit

--- client/cardapio/src/components/cardapioPizzas.jsx
+++ client/cardapio/src/components/cardapioPizzas.jsx
@@ -2,30 +2,76 @@ import React, {useState, useEffect} from 'react'
 import Axios from 'axios'
 
 const Cardapio = () =>{
-    const[categories, setCategories] =  useState([])
-    const[mixedCategories, setMixedCategories] =  useState([])
+
+    const allCategories = []
+    const AllProducts = []
+
+    const[category, setCategory] =  useState([])
+    const[categoryNames, setCategoryNames] =  useState([])
+    const[categorySizes, setCategorySizes] =  useState([])
+    const[productNames, setProductNames] = useState([])
+    const[productPrices, setProductPrices] = useState([])
+
     useEffect(()=>{
-        Axios.get('http://127.0.0.1:3001/api/getCategories').then((result)=>{
-            setCategories(result.data)
+        Axios.get(`http://127.0.0.1:3001/api/getCategoryNames`).then((result)=>{
+            setCategoryNames(result.data)
+        })
+        Axios.get(`http://127.0.0.1:3001/api/getCategorySizes`).then((result)=>{
+            setCategorySizes(result.data)
+        })
+        Axios.get(`http://127.0.0.1:3001/api/getProducts`).then((result)=>{
+            setProductNames(result.data)
+        })
+        Axios.get(`http://127.0.0.1:3001/api/getProductPrice`).then((result)=>{
+            setProductPrices(result.data)
         })
     }, [])
 
+                                
 
-    categories.map((category, index)=>{
-        Axios.get('http://127.0.0.1:3001/api/getSizes', {id_option: category.id}).then((result)=>{
-            setMixedCategories(result.data)
-            console.log(category.id)
+    categoryNames.map((categorySingle, index)=>{
+
+        const sizes = []
+
+        categorySizes.map((size, index)=>{
+            if(size.id_option == categorySingle.id){
+                sizes.push(size.name)
+            }
         })
+        allCategories.push({...categorySingle, sizes: [...sizes]})
     })
+
+
+    productNames.map((product, index)=>{
+        const prices = []
+        productPrices.map((price, index)=>{
+            if(price.id_produto == product.id){
+                prices.push(price.price)
+            }
+        })
+
+        AllProducts.push({...product, prices: [...prices]})
+    })
+
+    console.log(AllProducts)
+
     return (
         <>
-            {categories.map((category, index)=>{
+            {allCategories.map((categorySingle, index)=>{
+
+                var sizes = ``
+                
                 return (
 
                     <table>
                         <h2>
-                            { category.name }
+                            { categorySingle.name }
                         </h2>
+
+                        {categorySingle.sizes.map((size)=>{
+                            return (<th>{size}</th>)
+                        })}
+
                     </table>
                 
 
--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const app = express();
 const mysql = require("mysql")
 
-const db = mysql.createPool({
+const pool = mysql.createPool({
     host: "localhost",
     user: "root",
     password: "",
@@ -13,13 +13,79 @@ const db = mysql.createPool({
 
 app.use(cors())
 app.use(express.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: false}))
 
 
-app.get('/api/getCategories', (req, res)=>{
-    const sqlSelect = "SELECT * FROM categories"
-    db.query(sqlSelect, (err, result)=>{
-        res.send(result)
+
+/* SELECT c.name as c_name, p.name as p_name, op.name, pp.price FROM `categories` c INNER JOIN products p ON p.id_category = c.id INNER JOIN option_sizes op ON op.id_option = c.id INNER JOIN product_prices pp ON pp.id_produto = p.id; */
+
+
+
+
+app.get('/api/getCategoryNames', (req, res)=>{
+    console.log("DONE")
+    pool.getConnection((err, connection) => {
+        console.log(`CONNECT AS ID ${connection.threadId}`)
+        
+        connection.query("SELECT * FROM categories WHERE name not like 'vi%'", (err, result)=>{
+            connection.release()
+            if(!err){
+                res.send(result)
+                console.log("CONNECTED")
+            }else{
+                console.log(err)
+            }
+        })
+    })
+})
+
+
+app.get('/api/getCategorySizes/', (req, res)=>{
+    pool.getConnection((err, connection) => {
+        const sql = "SELECT * FROM `option_sizes`  ORDER BY `option_sizes`.`id_option` ASC"
+        connection.query(sql, (err, result)=>{
+            connection.release()
+            if(!err){
+                res.send(result)
+                console.log(result)
+            }else{
+                console.log(err)
+            }
+        })
+    })
+}) 
+
+
+
+app.get("/api/getProducts/", (req, res)=>{
+    pool.getConnection((err, connection)=>{
+        const sql = "SELECT * FROM `products`"
+        connection.query(sql, (err, result)=>{
+            connection.release()
+            if(!err){
+                res.send(result)
+                console.log(result)
+            }else{
+                console.log(err)
+            }
+        })
+    })
+})
+
+
+
+app.get("/api/getProductPrice/", (req, res)=>{
+    pool.getConnection((err, connection)=>{
+        const sql = "SELECT * FROM `product_prices`"
+        connection.query(sql, (err, result)=>{
+            connection.release()
+            if(!err){
+                res.send(result)
+                console.log(result)
+            }else{
+                console.log(err) 
+            }
+        })
     })
 })
 
@@ -27,6 +93,19 @@ app.get('/api/getCategories', (req, res)=>{
 
 
 
-app.listen("3001", () =>{
-    console.log("running on port 3001")
+
+app.listen(3001, () =>{
+    console.log("running on port ", 3001)
 })
+
+
+
+
+
+
+
+
+
+
+
+
